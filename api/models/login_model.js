f89c33e305542b70bb2a1492bf427674b092c58c
Tomi Lämsä <tomppa175@gmail.com>
2022-04-14 14:54:57 +0300

    k

--- api/models/login_model.js
+++ api/models/login_model.js
@@ -1,7 +1,7 @@
 const db = require('../database');
 
 const login={
-  checkPassword: function(username, callback) {
+  checkcardPIN: function(username, callback) {
       return db.query('SELECT password FROM pankkiautomaatti WHERE username = ?',[username], callback); 
     }
 };
